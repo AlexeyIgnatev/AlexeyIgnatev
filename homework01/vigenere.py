7646b06c9f68116547a368fea388a7848de2e182
AlexeyIgnatev <79508350832L@gmail.com>
2022-10-03 21:44:45 +0300

    vigenere

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,30 +1,44 @@
-def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    """
-    Encrypts plaintext using a Vigenere cipher.
+import caesar
 
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
-    """
+
+def encrypt_vigenere(plaintext: str, keyword: str) -> str:
+    while len(keyword) < len(plaintext):
+        keyword += keyword
+
+    low_range = range(ord('a'), ord('z') + 1)
+    high_range = range(ord('A'), ord('Z') + 1)
     ciphertext = ""
+
+    for i in range(len(plaintext)):
+        c = plaintext[i]
+        k = keyword[i]
+        if ord(k) in low_range:
+            shift = ord(k) - ord('a')
+        else:
+            shift = ord(k) - ord('A')
+        ciphertext += caesar.encrypt_caesar(c, shift)
+
     # PUT YOUR CODE HERE
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    """
-    Decrypts a ciphertext using a Vigenere cipher.
+    while len(keyword) < len(ciphertext):
+        keyword += keyword
 
-    >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> decrypt_vigenere("python", "a")
-    'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
-    """
+    low_range = range(ord('a'), ord('z') + 1)
+    high_range = range(ord('A'), ord('Z') + 1)
     plaintext = ""
-    # PUT YOUR CODE HERE
+
+    for i in range(len(ciphertext)):
+        c = ciphertext[i]
+        k = keyword[i]
+
+        if ord(k) in low_range:
+            shift = ord(k) - ord('a')
+        else:
+            shift = ord(k) - ord('A')
+
+        plaintext += caesar.decrypt_caesar(c, shift)
+
     return plaintext
