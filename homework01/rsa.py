6df93df6e8ed61bb9cbe4912eaa12ffdede2401c
AlexeyIgnatev <79508350832L@gmail.com>
2022-10-03 22:02:25 +0300

    Реализованы функции multiplicative_inverse() and generate_keypair()

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -19,15 +19,7 @@ def gcd(a: int, b: int) -> int:
 
 
 def multiplicative_inverse(e: int, phi: int) -> int:
-    """
-    Euclid's extended algorithm for finding the multiplicative
-    inverse of two numbers.
-
-    >>> multiplicative_inverse(7, 40)
-    23
-    """
-    # PUT YOUR CODE HERE
-    pass
+    return pow(e, -1, phi)
 
 
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
@@ -36,10 +28,10 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
     elif p == q:
         raise ValueError("p and q cannot be equal")
 
-    n = pq
+    n = p * q
     # PUT YOUR CODE HERE
 
-    phi = (p-1)(q-1)
+    phi = (p - 1) * (q - 1)
     # PUT YOUR CODE HERE
 
     # Choose an integer e such that e and phi(n) are coprime
