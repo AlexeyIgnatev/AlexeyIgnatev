7c0a157de36d9e5d60f35b842a92e19b605c6321
AlexeyIgnatev <79508350832L@gmail.com>
2022-12-07 13:10:17 +0300

    init

--- homework05/vkapi/wall.py
+++ homework05/vkapi/wall.py
@@ -4,10 +4,15 @@ import typing as tp
 from string import Template
 
 import pandas as pd
+import requests
 from pandas import json_normalize
-
 from vkapi import config, session
 from vkapi.exceptions import APIError
+from math import ceil
+
+domainVK = config.VK_CONFIG["domain"]
+access_token = config.VK_CONFIG["access_token"]
+v = config.VK_CONFIG["version"]
 
 
 def get_posts_2500(
@@ -20,7 +25,21 @@ def get_posts_2500(
     extended: int = 0,
     fields: tp.Optional[tp.List[str]] = None,
 ) -> tp.Dict[str, tp.Any]:
-    pass
+    code = """return API.wall.get({
+                '"owner_id": "owner_id"',
+                '"domain": "domain"',
+                '"offset": offset',
+                '"count": "1"',
+                '"filter": "filter"',
+                '"extended": extended',
+                '"fields": "fields"',
+                '"v": "v"'
+                });"""
+    post = requests.post(
+        url=f"{domainVK}/execute",
+        data={"code": code, "access_token": f"{access_token}", "v": f"{v}"},
+    )
+    return post.json()["response"]["items"]
 
 
 def get_wall_execute(
@@ -34,19 +53,13 @@ def get_wall_execute(
     fields: tp.Optional[tp.List[str]] = None,
     progress=None,
 ) -> pd.DataFrame:
-    """
-    Возвращает список записей со стены пользователя или сообщества.
+    offset_count = ceil(count / max_count)
+    response: tp.List[str] = []
+    for i in range(offset_count):
+        posts = get_posts_2500(
+            owner_id, domain, i * 2500, max_count, max_count, filter, extended, fields
+        )
+        response += posts
+        time.sleep(0.68)
+    return json_normalize(response)
 
-    @see: https://vk.com/dev/wall.get
-
-    :param owner_id: Идентификатор пользователя или сообщества, со стены которого необходимо получить записи.
-    :param domain: Короткий адрес пользователя или сообщества.
-    :param offset: Смещение, необходимое для выборки определенного подмножества записей.
-    :param count: Количество записей, которое необходимо получить (0 - все записи).
-    :param max_count: Максимальное число записей, которое может быть получено за один запрос.
-    :param filter: Определяет, какие типы записей на стене необходимо получить.
-    :param extended: 1 — в ответе будут возвращены дополнительные поля profiles и groups, содержащие информацию о пользователях и сообществах.
-    :param fields: Список дополнительных полей для профилей и сообществ, которые необходимо вернуть.
-    :param progress: Callback для отображения прогресса.
-    """
-    pass
